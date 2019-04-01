02033e188393d6a030183c23ef1d9dfc7ee4c4ac
tukangketikpapua <49145471+tukangketikpapua@users.noreply.github.com>
2019-04-01 23:24:46 +0900

    mpo
    Musik Papua Original

---
+++ index.php
@@ -0,0 +1,410 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+    <meta charset="utf-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+    <meta name="description" content="" />
+
+    <meta name="author" content="" />
+
+    <!--[if IE]>
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <![endif]-->
+
+    <title>Musik Papua Original</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+    <!-- BOOTSTRAP CORE STYLE  -->
+
+    <link href="assets2/css/bootstrap.css" rel="stylesheet" />
+
+    <!-- FONT AWESOME ICONS  -->
+
+    <link href="assets2/css/font-awesome.css" rel="stylesheet" />
+
+    <!-- CUSTOM STYLE  -->
+
+    <link href="assets2/css/style.css" rel="stylesheet" />
+
+    
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+    <script>
+
+        (adsbygoogle = window.adsbygoogle || []).push({
+
+        google_ad_client: "ca-pub-8499306528863712",
+
+        enable_page_level_ads: true
+
+        });
+
+    </script>
+
+    
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+	<script>
+
+     	(adsbygoogle = window.adsbygoogle || []).push({
+
+          google_ad_client: "ca-pub-8499306528863712",
+
+          enable_page_level_ads: true
+
+     	});
+	</script>
+
+
+
+    <!-- HTML5 Shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+
+    <!--[if lt IE 9]>
+
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="jumbotron btn-warning">
+
+        <div class="judul">
+
+            <h1>M P O</h1>
+
+            <p>(Musik Papua Original)</p>
+
+        </div>
+
+    </div>
+
+    <!-- LOGO HEADER END-->
+
+    <!-- <section class="menu-section"> -->
+
+    <nav class="navbar navbar-default">
+
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+
+                    <span class="sr-only">Toggle navigation</span>
+
+                    <span class="icon-bar"></span>
+
+                    <span class="icon-bar"></span>
+
+                    <span class="icon-bar"></span>
+
+                </button>
+
+            <a class="navbar-brand" href="#">Beranda</a>
+
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+                <ul class="nav navbar-nav">
+
+                    <li><a href="#">DAFTAR MPO</a></li>
+
+                    <li><a href="#">DAFTAR ARTIS MPO</a></li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+    <!-- MENU SECTION END-->
+
+    <!-- <div class="content-wrapper"> -->
+
+    <div class="container">
+
+        <div class="col-md-12">
+
+            <div class="awal">
+
+                <h1>SELAMAT DATANG</h1>
+
+                <h1>KO DI</h1>
+
+                <h1>KUMPULAN MPO</h1>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    
+
+
+
+    <div class="lagu">
+
+    <!-- </div> -->
+
+    <!-- <div class="content-wrapper"> -->
+
+    <div class="container">
+
+        <div class="col-md-12">
+
+            <div class="row">
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>Thumbnail label</h3>
+
+                        <p>...</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 1</h3>
+
+                        <p>Lagu 1</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 2</h3>
+
+                        <p>Lagu 2</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 3</h3>
+
+                        <p>Lagu 3</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 4</h3>
+
+                        <p>Lagu 4</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 5</h3>
+
+                        <p>Lagu 5</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 6</h3>
+
+                        <p>Lagu 6</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 7</h3>
+
+                        <p>Lagu 7</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-6 col-md-4">
+
+                    <div class="thumbnail btn-info">
+
+                    <img src="assets2/img/64-64.jpg" alt="">
+
+                    <div class="caption">
+
+                        <h3>ARTIS MPO 8</h3>
+
+                        <p>Lagu 8</p>
+
+                        <p><a href="#" class="btn btn-success" role="button"><i class="glyphicon glyphicon-play">Mainkan Musik</i></a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+
+                    </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+    <footer>
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-12">
+
+                    Published 2019 By CEO and Founder : <a href="#" target="_blank">Kristovel Edoway</a>
+
+                </div>
+
+
+
+            </div>
+
+        </div>
+
+    </footer>
+
+                    
+
+<script src="assets/plugins/jQuery/jquery-2.2.3.min.js"></script>
+
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- AdminLTE App -->
+
+<script src="assets/dist/js/app.min.js"></script>
+
+<!--data tables-->
+
+<script src="assets/plugins/datatables/jquery.dataTables.min.js"></script>
+
+<script src="assets/plugins/datatables/dataTables.bootstrap.min.js"></script>
+
+<script src="assets/plugins/datepicker/bootstrap-datepicker.js"></script>
+
+<script src="assets/plugins/select2/select2.full.min.js"></script>
+
+
+
+</body>
+
+</html>
